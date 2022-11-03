82d184fb915a19b60470f341ac08163e25a8a228
Hassan Usman <hassanusmanofficial@gmail.com>
2022-11-03 14:49:47 +0500

    change

--- src/main/java/com/usermanagement/entity/Role.java
+++ src/main/java/com/usermanagement/entity/Role.java
@@ -25,7 +25,7 @@ public class Role implements Serializable {
     private int id;
     @Column(name = "name")
     private String name;
-    @Column(name = "description")  
+    @Column(name = "description")   
     private String description; 
 
 }
